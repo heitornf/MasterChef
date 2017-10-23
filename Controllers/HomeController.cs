7fd4c8b97f3a84aaf784abe955653473a13bdf03
heitornf <heitornf@gmail.com>
2017-10-23 00:59:48 -0200

    Funcoes de Incluir e Listar nas Controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,18 +23,38 @@ namespace MasterChef.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult IndexCadastroReceita(FormCollection collection)
+        {
+            var receita = new Receita();
+            TryUpdateModel(receita);
+            var recrep = new Repository.ReceitaRepository();
+            recrep.Incluir(receita);
+            return RedirectToAction("IndexListarReceita");
+        }
+        [HttpPost]
+        public ActionResult IndexCadastroCategoria(FormCollection collection)
+        {
+            var categ = new Categoria();
+            TryUpdateModel(categ);
+            var catrep = new Repository.CategoriaRepository();
+            catrep.Incluir(categ);
+            return RedirectToAction("IndexListarCategoria");
+        }
 
         public ActionResult IndexListarReceita(string prCadastroReceita)
         {
             List<Receita> ReceitaCollection = new List<Receita>();
-
+            var recrep = new Repository.ReceitaRepository();
+            ReceitaCollection = recrep.Listar().ToList();
             return View(ReceitaCollection);
         }
 
         public ActionResult IndexListarCategoria(string prCadastroReceita)
         {
             List<Categoria> CategoriaCollection = new List<Categoria>();
-
+            var catrep = new Repository.CategoriaRepository();
+            CategoriaCollection = catrep.Listar().ToList();
             return View(CategoriaCollection);
         }
     }
